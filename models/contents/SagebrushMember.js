7c8a34edf97c3fa2cc7887c7f6b202b619082ad2
lesly <leslyrevenge@gmail.com>
2022-03-01 19:07:23 -0500

    fixed model issue

---
+++ models/contents/SagebrushMember.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const TheSchema = new Schema({
+  fullname: String,
+  email: String,
+
+  published_on: {
+    type: Date,
+    default: Date.now,
+  },
+  // universal
+  public: {
+    type: Boolean,
+    default: false,
+  },
+  published: {
+    type: Boolean,
+    default: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+  },
+});
+
+module.exports = mongoose.model("SagebrushMember", TheSchema);
